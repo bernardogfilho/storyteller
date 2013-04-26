966920fb83434de3ca8dbbd4ba3d9bf16f8fb906
Bernardo Gurgel <bernardogfilho@gmail.com>
2013-04-25 23:40:00 -0300

    Deploy on heroku

--- config/application.rb
+++ config/application.rb
@@ -42,11 +42,6 @@ module UStar
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
 
-    config.generators do |g|
-        g.stylesheets false
-        g.helper false
-    end
-
     # Use SQL instead of Active Record's schema dumper when creating the database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,
     # like if you have constraints or database-specific column types
