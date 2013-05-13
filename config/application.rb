fe3d88556c7d04ebc2dd03c36ae7e3fec0d9016b
Bernardo Gurgel <bernardogfilho@gmail.com>
2013-05-13 08:59:11 -0300

    Deploy on Heroku

--- config/application.rb
+++ config/application.rb
@@ -56,6 +56,8 @@ module UStar
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    config.assets.precompile += %w( *.js *.css )
+
     config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
